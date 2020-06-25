6f78c4671bef5dd707a75547b0554b5f770d7cdd
MichaelCalabro <mmc.calabro@gmail.com>
2020-06-25 10:03:54 -0400

    Updates

---
+++ precache-manifest.7a47e12b9307c3bcc3b1d633b366b4a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32649abe364f705e4d6ecfb287f0ac2f",
+    "url": "/Chronicles/index.html"
+  },
+  {
+    "revision": "53e041013d2de9448b84",
+    "url": "/Chronicles/static/css/main.a062fb49.chunk.css"
+  },
+  {
+    "revision": "1623a7a5e4a4e13be8b3",
+    "url": "/Chronicles/static/js/2.b97980e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chronicles/static/js/2.b97980e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e041013d2de9448b84",
+    "url": "/Chronicles/static/js/main.22a511ba.chunk.js"
+  },
+  {
+    "revision": "df5e14876ede7cdba572",
+    "url": "/Chronicles/static/js/runtime-main.20e8cc76.js"
+  },
+  {
+    "revision": "3bb770c62052335c46f0c6c379b9ae9c",
+    "url": "/Chronicles/static/media/Onciale PhF01.3bb770c6.ttf"
+  },
+  {
+    "revision": "d572fe14a97d65639e260215c0beacb0",
+    "url": "/Chronicles/static/media/man.d572fe14.jpg"
+  },
+  {
+    "revision": "dc6d25f2ffa25d073c3c5387dc77a450",
+    "url": "/Chronicles/static/media/painting.dc6d25f2.jpg"
+  },
+  {
+    "revision": "6e39b48b6faf9e4f0d6f8ac98e06a2b7",
+    "url": "/Chronicles/static/media/scripture.6e39b48b.jpg"
+  }
+]);
